419bcfafeb004b8d74666a539e8eb73303abffe2
Yosri-Jemai <yosrii.jemaiii@gmail.com>
2025-03-08 22:59:42 +0100

    weather icons

--- weatherAPI.js
+++ weatherAPI.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchText = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function fetchWeatherData(city){
     const weatherData = await fetch(apiUrl+city+`&appid=${apiKey}`);
@@ -12,6 +13,20 @@ async function fetchWeatherData(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
     document.querySelector(".wind").innerHTML = data.wind.speed+" km/h";
+    
+    if (data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }else if (data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }else if (data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }else if (data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }else if (data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    
+    //console.log(data);
 }
 
 searchBtn.addEventListener("click", () => {
